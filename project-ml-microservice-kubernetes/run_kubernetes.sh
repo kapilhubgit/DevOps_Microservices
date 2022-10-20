5b1ae50023ab342341dd1d800cad82f98bf36e65
EC2 Default User <ec2-user@ip-172-31-44-29.ec2.internal>
2022-10-20 21:00:41 +0000

    Udacity Project 4

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+ dockerpath=kapildockerlogin/udacity-project4-microservices
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity-project4-microservices --image=$dockerpath --port=80 --labels app=udacityproject4
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
-
+# Forward the container port to a host.
+kubectl port-forward udacity-project4-microservices 8000:80
